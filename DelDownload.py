9364588491e0196967f04e87a5271a5e575f9477
shymiY3 <szymonjanski1@gmail.com>
2023-03-22 18:50:02 +0100

    Final branch commit

--- DelDownload.py
+++ DelDownload.py
@@ -1,17 +1,25 @@
 import os, time
-from sys import argv
 from send2trash import send2trash
-import argparse
+from argparse import ArgumentParser
 
+parser = ArgumentParser(description='Delete files in directory whiches modification date is older than 30 days')
+parser.add_argument('-p', '--path',  type=str, default='', help='Path to directory to remove, default: current')
+parser.add_argument('-d', '--mdays',  type=int, default=30, help='Max days without modification, default: 30')
+parser.add_argument('-l', '--log',  type=str, default='folldd', help='Path to store log, default: removing files directory')
+args = parser.parse_args()
 
-PATH = fr'{argv[1]}'
-DAYS = int(argv[2])*60*60*24
+PATH = fr'{args.path}'
+DAYS = args.mdays*60*60*24
+if not args.log:
+    LOG = PATH
+else: LOG = args.log
 
 RemFiles = []
 
+print(PATH)
 try:
-    with open(os.path.join(PATH, 'DelFiles.log'), 'a+', encoding='UTF-8') as DelFiles:
-        for file in os.listdir(PATH):
+    with open(os.path.join(LOG, 'DelFiles.log'), 'a+', encoding='UTF-8') as DelFiles:
+        for file in eval(f'os.listdir({PATH})'):
             if (time.time() > (modtime:=os.path.getmtime(os.path.join(PATH, file)))+DAYS) and file != 'DelFiles.txt':
                 send2trash(os.path.join(PATH, file))
                 RemFiles.append(time.strftime('%Y-%m-%d, %H:%M:%S', time.localtime(modtime)) + '\t' + file)
